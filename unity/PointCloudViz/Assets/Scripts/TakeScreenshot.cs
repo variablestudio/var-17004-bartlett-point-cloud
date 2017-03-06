fc3e6390e842f8b0e364edbd6d06f33376d8f9e8
Marcin Ignac <marcin.ignac@gmail.com>
2017-03-06 16:40:04 +0000

    Add TakeScreenshot script

---
+++ unity/PointCloudViz/Assets/Scripts/TakeScreenshot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TakeScreenshot : MonoBehaviour {
+
+	public int Resolution = 8;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Application.CaptureScreenshot("Screenshot_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png", Resolution);
+		}
+
+	}
+
+}
